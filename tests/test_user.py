112e790f3814f8929727a80d07d33d7041b0986e
NormanwOw <normjkeee@vk.com>
2024-01-27 14:07:30 +0500

    v1.3.5 -added tests for anonym client

--- tests/test_user.py
+++ tests/test_user.py
@@ -11,62 +11,17 @@ from tests.conftest import engine_test
 class TestUserAuth:
 
     @staticmethod
-    @pytest.mark.parametrize(
-        'username, email, pw1, pw2, msg, code', [
-            ('example', 'example@gmail.com', 'stringstring', 'stringstring', None, 201),
-            ('example', 'examplegmail.com', 'stringstring', 'stringstring', 'email', 422),
-            ('example', 'example@gmail.com', 'stringstring', 'string', 'Passwords', 422),
-            ('ex', 'example@gmail.com', 'stringstring', 'stringstring', 'username', 422),
-            ('example@', 'example@gmail.com', 'stringstring', 'stringstring', 'username',
-             422),
-            ('example', 'example@gmail.com', 'st', 'st', 'password', 422),
-        ]
-    )
-    async def test_registration(ac: AsyncClient, username, email, pw1, pw2, msg, code):
-        response = await ac.post('api/v1/auth/registration', json={
-            'username': username,
-            'email': email,
-            'password': pw1,
-            'confirm_password': pw2
-        })
-
-        assert response.status_code == code
-        detail = response.json().get('detail')
-        if detail:
-            assert msg in detail.get('msg')
-
-    @staticmethod
-    @pytest.mark.parametrize(
-        'username, pw, msg, code', [
-            ('example', 'stringstring', 'success', 200),
-            ('examplee', 'stringstring', 'Username', 404),
-            ('example', 'asdasdasd', 'Password', 422),
-        ]
-    )
-    async def test_login(ac: AsyncClient, username, pw, msg, code):
-        response = await ac.post(url='api/v1/auth/login', json={
-            'username': username,
-            'password': pw
-        })
-
-        assert response.status_code == code
-        detail = response.json().get('detail')
-        if detail:
-            if isinstance(detail, dict):
-                assert msg in detail.get('msg')
-            elif isinstance(detail, str):
-                assert msg in detail
-
-            if response.status_code == 200:
-                assert ac.cookies.get(SITE_NAME) is not None
-
-    @staticmethod
-    async def test_logout(ac: AsyncClient):
-        response = await ac.post(url='api/v1/auth/logout')
+    async def test_logout(authorized_client: AsyncClient):
+        response = await authorized_client.post(url='api/v1/auth/logout')
 
         assert response.status_code == 200
         assert 'success' in response.json().get('detail')
-        assert ac.cookies.get(SITE_NAME) is None
+        assert authorized_client.cookies.get(SITE_NAME) is None
+
+        await authorized_client.post('api/v1/auth/login', json={
+            'username': 'username',
+            'password': 'stringstring'
+        })
 
 
 class TestUserUsers:
